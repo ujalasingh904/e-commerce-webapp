ea037079503d1bcd3cb4d45d5a13810f5254f9a0
ujala <singhujala2004@gmail.com>
2025-12-29 15:08:06 +0530

    Add new components and enhance existing ones for improved e-commerce functionality
    - Implemented CartModal for managing cart items and checkout process.
    - Added CategoryList for displaying product categories.
    - Introduced Filter component for product filtering options.
    - Enhanced Navbar with search functionality and navigation icons.
    - Created ProductList with pagination for displaying products.
    - Developed Slider for showcasing promotional content.
    - Updated Footer with company information and subscription options.
    - Improved HomePage layout with featured products and categories.
    - Added styles for scrollbar and custom themes in globals.css.
    - Updated tailwindcss dependency to version 4.1.18.
    - Configured remote image patterns in next.config.ts for optimized image loading.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "people.pic1.co",
+      },
+      {
+        protocol: "https",
+        hostname: "app-uploads-cdn.fera.ai",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import Menu from "./Menu";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
 
 export default function Navbar() {
     return (
         <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
-                {/* mobile */}
-            <div className="h-full flex items-center justify-between">
-
-
+            {/* mobile */}
+            <div className="h-full flex items-center justify-between md:hidden">
                 <Link href={"/"}>
                     <div className="text-2xl tracking-wide">ShopEase</div>
                 </Link>
@@ -17,9 +18,24 @@ export default function Navbar() {
             {/* bigger screens */}
             <div className="hidden md:flex items-center justify-between gap-8 h-full">
                 {/* left */}
-                <div className="w-1/3"></div>
+                <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
+                    <Link href="/" className="flex items-center gap-2">
+                        <Image src="/logo.png" alt="" width={24} height={24} />
+                        <div className="text-2xl tracking-wide">ShopEase</div>
+                    </Link>
+                    <div className="hidden xl:flex gap-4">
+                        <Link href="/">Homepage</Link>
+                        <Link href="/">Shop</Link>
+                        <Link href="/">Deals</Link>
+                        <Link href="/">About</Link>
+                        <Link href="/">Contact</Link>
+                    </div>
+                </div>
                 {/* right */}
-                <div className="w-2/3"></div>
+                <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
+                    <SearchBar />
+                    <NavIcons />
+                </div>
             </div>
         </div>
     )
